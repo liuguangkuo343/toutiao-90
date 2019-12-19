e31029b428e6d8e3fdd03c4c01602ce0bcaa33ec
liuguangkuo343 <liuguangkuo343>
2019-12-19 21:26:36 +0800

    黑马头条PC-主页模板-页面布局

--- src/views/home/index.vue
+++ src/views/home/index.vue
@@ -1,13 +1,21 @@
 <template>
-  <div>主页</div>
+  <!-- 整体的容器 -->
+  <el-container>
+    <!-- 左右布局 -->
+    <el-aside>
+      <el-row></el-row>
+    </el-aside>
+    <!-- 上下布局 -->
+    <el-container>
+      <el-header>头部容器</el-header>
+    <el-main>主要内容</el-main>
+    </el-container>
+
+  </el-container>
 </template>
-
 <script>
-export default {
-
-}
+export default {}
 </script>
 
 <style>
-
 </style>
