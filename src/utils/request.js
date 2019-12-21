d7a75f06544409263e69b73f309c014d67cde68a
liuguangkuo343 <liuguangkuo343>
2019-12-21 20:43:46 +0800

    黑马头条PC-主页模块-请求拦截器统一处理响应数据

--- src/utils/request.js
+++ src/utils/request.js
@@ -1,3 +1,5 @@
+// 请求拦截器
+
 // 封装axios
 import axios from 'axios'
 // 请求拦截 config 配置项
@@ -7,5 +9,15 @@ axios.interceptors.request.use(function (config) {
   return config // 返回后台操作
 }, function (params) {
 
+})
+
+// 响应拦截器
+
+axios.interceptors.response.use(function (response) {
+//   成功执行函数  统一处理状态码
+
+  return response.data ? response.data : {}
+}, function name (params) {
+// 失败时候执行
 })
 export default axios
--- src/views/login/index.vue
+++ src/views/login/index.vue
@@ -74,7 +74,7 @@ export default {
             data: this.loginForm // body参数
           }).then(result => {
             // 只接受正确结果
-            window.localStorage.setItem('user-token', result.data.data.token)
+            window.localStorage.setItem('user-token', result.data.token)
             this.$router.push('/home')
             // console.log(result.data.data.token)
           }).catch(() => {
