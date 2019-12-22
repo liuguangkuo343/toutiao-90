f66d7cb4c2f33536a5f48668d523e0a8309f6e3b
liuguangkuo343 <liuguangkuo343>
2019-12-22 09:15:06 +0800

    黑马头条PC-评论列表-页面结构-table

--- src/components/index.js
+++ src/components/index.js
@@ -1,10 +1,10 @@
 import Homeheader from './home/home-header.vue'
 import Leftaside from './home/leftaside.vue'
-// import breadCrumb from './common/bread-crumb'
+import breadcrumb from './common/bread-crumb'
 export default {
   install (Vue) {
     Vue.component('home-header', Homeheader)
     Vue.component('leftaside', Leftaside)
-    // Vue.breadCrumb('bread-crumb', breadCrumb) // 全局注册面包屑
+    Vue.component('bread-crumb', breadcrumb)
   }
 }
