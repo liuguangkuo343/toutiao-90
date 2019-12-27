13b785ad14a6402dfe6338f22c11812080b39b52
liuguangkuo343 <liuguangkuo343>
2019-12-27 22:56:09 +0800

    黑马头条PC-账户信息-页面结构

--- src/views/account/index.vue
+++ src/views/account/index.vue
@@ -2,9 +2,31 @@
   <el-card>
       <bread-crumb slot="header">
         <template slot="title">
-            娃哈哈
+            账户信息
         </template>
       </bread-crumb>
+      <!-- 表单容器 -->
+      <el-form style="margin-left:100px" label-width="100px">
+                <el-form-item label="用户名:">
+                    <el-input style="width:35%"></el-input>
+                    </el-form-item>
+                <el-form-item label="简介:" >
+                    <el-input style="width:35%"></el-input>
+                    </el-form-item>
+                <el-form-item label="邮箱:" >
+                    <el-input style="width:35%"></el-input>
+                    </el-form-item>
+                <el-form-item label="手机号:" >
+                    <el-input disabled style="width:35%"></el-input>
+                    </el-form-item>
+                <el-form-item >
+                    <el-button type="primary"> 保存信息</el-button>
+                </el-form-item>
+            </el-form>
+            <!-- 上传组件 -->
+            <el-upload action="" :show-file-list="false" class="header-upload">
+                <img src="../../assets/img/dijia.jpg" alt="">
+            </el-upload>
   </el-card>
 </template>
 
@@ -14,6 +36,15 @@ export default {
 }
 </script>
 
-<style>
-
+<style lang="less" scoped>
+    .header-upload{
+       position: absolute;
+       right: 300px;
+       top: 200px;
+       img{
+              width: 200px;
+        height: 200px;
+            border-radius: 50%
+       }
+    }
 </style>
